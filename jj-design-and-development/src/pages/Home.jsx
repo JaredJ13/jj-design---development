5006bc8af394382ade166ced1067b8d5f3b8604b
Jared Jahnke <77257554+JaredJ13@users.noreply.github.com>
2023-07-05 20:22:55 -0600

    Initial vite app & structure

---
+++ jj-design-and-development/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import CustomHeader from "../components/customHeader"
+
+export default function Home() {
+    return (
+        <>
+            <h1>hi</h1>
+            <CustomHeader name='Jared' />
+        </>
+    )
+}
