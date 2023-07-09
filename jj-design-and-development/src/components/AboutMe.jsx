5310e624753d0091d8c0ed5085be9ad3d7a321fc
Jared Jahnke <77992735+IJaredI@users.noreply.github.com>
2023-07-08 18:03:11 -0600

    About Me section major styling/layout

--- jj-design-and-development/src/components/AboutMe.jsx
+++ jj-design-and-development/src/components/AboutMe.jsx
@@ -1,20 +1,89 @@
 // import styled components
-import { H2 } from '../styles/styled-components/all-styled-components';
+import {
+    H2,
+    Container,
+    InnerContainer,
+    H2TitleContainer,
+    AboutIcon,
+    AboutParagraph,
+    Flex,
+    AboutIconExtended,
+    H3,
+    MySkillsContainer,
+    MySkillsContainerRow
+} from '../styles/styled-components/all-styled-components';
 
 // import icons
 import ProgrammingIcon from '../assets/white-icons/icons8-programming-100.png';
+import GymIcon from '../assets/white-icons/icons8-pullups-100.png';
+import ChessIcon from '../assets/white-icons/icons8-king-100.png';
+import MusicIcon from '../assets/white-icons/icons8-music-heart-100.png';
+import JSIcon from '../assets/white-icons/icons8-js-100.png';
+import ReactIcon from '../assets/white-icons/icons8-react-native-100.png';
+import NodeJSIcon from '../assets/white-icons/icons8-nodejs-100.png';
+import CsIcon from '../assets/white-icons/icons8-cs-100.png';
+import AspIcon from '../assets/white-icons/icons8-asp-100.png';
+import HtmlIcon from '../assets/white-icons/icons8-html-100.png';
+import CssIcon from '../assets/white-icons/icons8-css3-100.png';
+import BootstrapIcon from '../assets/white-icons/icons8-bootstrap-100.png';
+import MySqlIcon from '../assets/white-icons/icons8-mysql-100.png';
+import FirebaseIcon from '../assets/white-icons/icons8-firebase-100.png';
+import OracleIcon from '../assets/white-icons/icons8-oracle-logo-100.png';
+import GithubIcon from '../assets/white-icons/icons8-github-100.png';
+import FigmaIcon from '../assets/white-icons/icons8-figma-100.png';
 
 export default function AboutMe() {
     return (
         <>
-            <section className='about-container'>
-                <div className='about-inner-container'>
-                    <div className='about-title-container'>
+            <Container>
+                <InnerContainer>
+                    <H2TitleContainer>
                         <H2>About Me</H2>
-                    </div>
-                    <img src={ProgrammingIcon} alt='Desktop monitor with coding icon' />
-                </div>
-            </section>
+                    </H2TitleContainer>
+                    <AboutIcon src={ProgrammingIcon} alt='Desktop monitor with coding icon' />
+                    <AboutParagraph>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </p>
+                    </AboutParagraph>
+                    <Flex>
+                        <AboutIconExtended src={GymIcon} alt='Stick figure doing pullup' />
+                        <AboutIconExtended src={ChessIcon} alt='King chess piece' />
+                        <AboutIconExtended src={MusicIcon} alt='Musice symbol inside the outline of a heart' />
+                    </Flex>
+                    <AboutParagraph>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                        </p>
+                    </AboutParagraph>
+                    <H3>- My Skills -</H3>
+                    <MySkillsContainer>
+                        <MySkillsContainerRow>
+                            <img src={JSIcon} alt='JavaScript icon' />
+                            <img src={ReactIcon} alt='ReactJS icon' />
+                            <img src={NodeJSIcon} alt='NodeJS icon' />
+                        </MySkillsContainerRow>
+                        <MySkillsContainerRow>
+                            <img src={CsIcon} alt='C# icon' />
+                            <img src={AspIcon} alt='Asp icon' />
+                        </MySkillsContainerRow>
+                        <MySkillsContainerRow>
+                            <img src={HtmlIcon} alt='HTML icon' />
+                            <img src={CssIcon} alt='CSS3 icon' />
+                            <img src={BootstrapIcon} alt='Bootstrap framework icon' />
+                        </MySkillsContainerRow>
+                        <MySkillsContainerRow>
+                            <img src={MySqlIcon} alt='MySQL icon' />
+                            <img src={FirebaseIcon} alt='Google Firebase icon' />
+                            <img src={OracleIcon} alt='Oracle icon' />
+                        </MySkillsContainerRow>
+                        <MySkillsContainerRow>
+                            <img src={GithubIcon} alt='Github icon' />
+                            <img src={FigmaIcon} alt='Figma icon' />
+                        </MySkillsContainerRow>
+                    </MySkillsContainer>
+                </InnerContainer>
+            </Container>
         </>
     )
 }
--- jj-design-and-development/src/styles/styled-components/all-styled-components.jsx
+++ jj-design-and-development/src/styles/styled-components/all-styled-components.jsx
@@ -1,9 +1,83 @@
 import styled from 'styled-components'
 
+// *********Container Components*********
+export const Container = styled.section`
+font-family: 'rufina', serif;
+font-family: ${(props) => props.fontFamily};
+    width: 100%;
+`
+
+export const InnerContainer = styled.div`
+    width: 70%;
+    margin: 0 auto;
+`
+
+// *********Flex Components*********
+export const Flex = styled.div`
+    display: flex;
+    justify-content: center;
+    justify-content: ${(props) => props.justifyContent};
+`
+
+
 // *********About Me Page Components*********
+export const H2TitleContainer = styled.div`
+    width: 386px;
+    background-color: #000000;
+    border-bottom: 1px solid #afe1f6;
+    margin: 0 auto;
+`
 
 export const H2 = styled.h2`
     color: #afe1f6;
     color: ${(props) => props.color};
-    font-size: 32px;
-` 
+    font-size: 22px;
+    text-align: center;
+`
+
+export const AboutIcon = styled.img`
+    width: 4rem;
+    margin: 3rem auto 1rem auto;
+`
+
+export const AboutIconExtended = styled(AboutIcon)`
+    margin: 2rem 0 1rem 0;
+`
+
+export const MySkillsContainer = styled.div`
+    width: 50rem;
+    margin: 0 auto;
+
+    img{
+        width: 8rem;
+    }
+`
+export const MySkillsContainerRow = styled.div`
+    display: flex;
+    align-items: center;
+    width: 50%;
+    margin: 0 auto 1.5rem auto;
+    padding-bottom:1rem ;
+    border-bottom: 1px solid #afe1f6;
+`
+
+export const AboutParagraph = styled.div`
+    width: 80rem;
+    height: 100%;
+    margin: 0 auto 0rem auto;
+    text-align: center;
+    border-top: 1px solid #afe1f6;
+
+    p{
+        font-family: 'Oxygen', sans-serif;
+    }
+`
+
+export const H3 = styled.h3`
+    font-family: 'Oxygen', sans-serif;
+    color: #dabfa7;
+    text-align: center;
+    font-size: 22px;
+    margin-top: 5rem;
+    margin: ${(props) => props.margin};
+`
